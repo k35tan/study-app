9f54754a867a8a6cfc9964d9e7ca996db5a18486
k35tan <k35tan@uwaterloo.ca>
2021-01-16 19:40:04 -0500

    More changes

--- src/main-page/mainPage.jsx
+++ src/main-page/mainPage.jsx
@@ -2,7 +2,8 @@ import React, { useState, useEffect, Component } from "react";
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import { Player } from "video-react";
-import {TestApi} from '../api/api.js';
+import { TestApi } from '../api/api.js';
+import videoPlaceholder from '../../src/video-placeholder.png';
 
 import './mainPage.css';
 import "./../../node_modules/video-react/dist/video-react.css";
@@ -57,27 +58,58 @@ class MainPage extends Component {
     render() {
         return (
             <div className='main-page-container'>
-                <h1>Main Page File</h1>
-
-                <Form>
-                    <Form.Group>
-                        <Form.File id="exampleFormControlFile1" onChange={e => {
-                            this.handleVideoLoad(e);
-                        }} />
-                    </Form.Group>
-                </Form>
-
-                <Button onClick={() => this.anotherAPI()}>Call api</Button>
-                <div>{this.state.first}</div>
-                <div>{this.state.second}</div>
-
-                <Player
-                    playsInline
-                    src={this.state.videoFileURL}
-                    fluid={false}
-                    width={480}
-                    height={272}
-                />
+                <div className='left-side'>
+                    {/* <h1 className='title'>STUDY APP</h1> */}
+                    <Player
+                        playsInline
+                        poster={videoPlaceholder}
+                        src={this.state.videoFileURL}
+                        fluid={false}
+                        width={700}
+                        height={457}
+                    />
+                    <Form>
+                        <Form.Group>
+                            <Form.File id="exampleFormControlFile1" onChange={e => {
+                                this.handleVideoLoad(e);
+                            }} />
+                        </Form.Group>
+                    </Form>
+                </div>
+                <div className='right-side'>
+                    <div className="timestamp-container">
+                        <div className="keyword-input-container">
+                            <Form.Group  >
+                                <Form.Control className="keyword-input" type="text" placeholder="Enter a keyword" />
+                                <Button className="grey-btn" onClick={() => this.anotherAPI()}>Search</Button>
+                            </Form.Group>
+                        </div>
+                        <div className="text-container">
+                            <div>{this.state.first}</div>
+                            <div>{this.state.second}</div>
+                            <div>{this.state.first}</div>
+                            <div>{this.state.first}</div>
+                            <div>{this.state.second}</div>
+                            <div>{this.state.first}</div>
+                            <div>{this.state.second}</div>
+                            <div>{this.state.first}</div>
+                            <div>{this.state.first}</div>
+                            <div>{this.state.second}</div>
+                            <div>{this.state.first}</div>
+                            <div>{this.state.first}</div>
+                            <div>{this.state.second}</div>
+                            <div>{this.state.first}</div>
+                            <div>{this.state.first}</div>
+                            <div>{this.state.second}</div>
+                            <div>{this.state.first}</div>
+                            <div>{this.state.second}</div>
+                            <div>{this.state.first}</div>
+                            <div>{this.state.first}</div>
+                            <div>{this.state.second}</div>
+                            <div>{this.state.first}</div>
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
